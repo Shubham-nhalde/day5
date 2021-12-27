6a8aae96fc4d1fc9adf83baeb4aa62acd08b593d
Shubham <nhaldes@gmail.com>
2021-12-27 23:13:47 +0530

    add 3leapyear.sh

---
+++ 3leapyear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+read -p "Enter Year : " year
+
+if [ $(($year % 400)) -eq 0 ]
+then
+   echo " $year is a leap year "
+
+elif [ $(($year %4 )) -eq 0 ]
+then
+   echo " $year is a leap year "
+
+elif [ $(($year % 100)) -eq 0 ]
+then
+   echo " $year is not a leap year "
+
+fi
